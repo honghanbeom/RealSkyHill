6c42e410968e29635b0b69ed63a570855cdada7d
MYNOTEBOOK\bobby <bobbyhong7@naver.com>
2023-08-10 14:31:49 +0900

    Feat : Script Added
    ID Classify Script Added, CSV Reader Added(main)
    SpriteMatch Script need to be update
    Test1 just for test
    
    #Resolves : #IssueNo
    #See Also : #IssueNo

---
+++ SkyHill/Assets/HHB/HHB_Scripts/SpriteMatch.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMatch : MonoBehaviour
+{
+    public Sprite[] emergencySprites;
+    public Sprite[] vipSprites;
+    public Sprite[] makingWeaponSprites;
+    public Sprite[] rootWeaponSprites;
+    public Sprite[] makingMaterialSprites;
+    public Sprite[] rootMaterialSprites;
+    public Sprite[] freshFoodSprites;
+    public Sprite[] spoiledFoodSprites;
+    public Sprite[] rootFoodSprites;
+    public Sprite[] makingFoodSprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
