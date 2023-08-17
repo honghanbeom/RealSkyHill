31938066e9e80ba19e99aa2c6227f69ad13354c3
정현우\jhw50 <jhw504166@gmail.com>
2023-08-17 17:45:54 +0900

    feat : fight ui and Monster flip
    Fight Ui Bug Fix and Monster Flip Bug Fix
    
    Resiolves : Fight Ui Bug Fix and Monster Flip Bug Fix
    
    See Also : #issueNo

--- SkyHill/Assets/JHW/Assets/Scripts/MosterSpawn.cs
+++ SkyHill/Assets/JHW/Assets/Scripts/MosterSpawn.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class MosterSpawn : MonoBehaviour
 {
-    public GameObject rookiePreafab;
-    public GameObject neighbourPreafab;
+    public GameObject uiImage; // 활성화할 UI 이미지
+    public GameObject uiImage1; // 활성화할 UI 이미지
+    public GameObject uiImage2; // 활성화할 UI 이미지
 
     public GameObject rookiePreafab_R;
     public GameObject neighbourPreafab_R;
@@ -33,7 +34,24 @@ public class MosterSpawn : MonoBehaviour
     {
         ReferenceMonster();
         SpawnRandomPrefab();
+        uiImage = GameObject.Find("LeftHand");
+        uiImage1 = GameObject.Find("RightHand");
+        uiImage2 = GameObject.Find("AttackType");
 
+        ImageSc();
+        ImageSc1();
+        ImageSc2();
+
+
+
+
+
+
+
+
+        //uiImage.SetActive(false);
+        //uiImage1.SetActive(false);
+        //uiImage2.SetActive(false);
         //rookiePreafab_L = rookiePreafab;
         //rookiePreafab_L.transform.localScale = new Vector3(-rookiePreafab_L.transform.localScale.x, rookiePreafab_L.transform.localScale.y, rookiePreafab_L.transform.localScale.z);
 
@@ -47,13 +65,14 @@ public class MosterSpawn : MonoBehaviour
 
 
 
-        int randomIndex = Random.Range(0, 2); // 랜덤 인덱스 선택
 
         for (int x = 0; x < monsterCount.x; x++)
         {
 
             for (int y = 0; y < monsterCount.y; y++)
             {
+                int randomIndex = Random.Range(0, 2); // 랜덤 인덱스 선택
+                int randomSpawn = Random.Range(0, 10);
 
                 //Rookie 영점 조절
                 float xPos = (spawnPoint.x * x) - 1.12f;
@@ -67,17 +86,22 @@ public class MosterSpawn : MonoBehaviour
 
                 Vector3 neighbourSpwanPoint = new Vector3(xPos1, yPos1, 0);
 
-
-                if (randomIndex == 0)
+                if (randomSpawn < 3)
                 {
-                    Instantiate(rookiePreafab, rookieSpwanPoint, Quaternion.identity);
+
+                    if (randomIndex == 0)
+                    {
+                        Instantiate(rookiePreafab_L, rookieSpwanPoint, Quaternion.identity);
+                    }
+                    else if (randomIndex == 1)
+                    {
+                        //NeighbourPrefabFlip();
+                        Instantiate(neighbourPreafab_L, neighbourSpwanPoint, Quaternion.identity);
+                        //NeighbourPrefabFlip();
+                    }
                 }
-                else if (randomIndex == 1)
+                else if (randomSpawn >= 3)
                 {
-                    NeighbourPrefabFlip();
-                    Instantiate(neighbourPreafab, neighbourSpwanPoint, Quaternion.identity);
-                    NeighbourPrefabFlip();
-
 
                 }
 
@@ -93,111 +117,161 @@ public class MosterSpawn : MonoBehaviour
     {
 
 
-        int randomIndex = Random.Range(0, 2); // 랜덤 인덱스 선택
-        int randomRL = Random.Range(0, 2); // 랜덤 인덱스 선택
 
         for (int x = 0; x < monsterCount.x; x++)
         {
 
             for (int y = 0; y < monsterCount.y; y++)
             {
+                int randomIndex = Random.Range(0, 2); // 랜덤 인덱스 선택
+                int randomRL = Random.Range(0, 2); // 랜덤 인덱스 선택
+                int randomSpawn1 = Random.Range(0, 10);
 
                 //Rookie 영점 조절
                 float xPos = (spawnPoint.x * x) - 1.12f;
                 float yPos = (spawnPoint.y * -y) - 2.1f;
 
+                //LeftRookie 영점 조절
+                float xPos4 = (spawnPoint.x * x) + 1.5f;
+                float yPos4 = (spawnPoint.y * -y) - 2.1f;
+
                 //Neighbour 영점 조절 
                 float xPos1 = (spawnPoint1.x * x) - 5.2f;
                 float yPos1 = (spawnPoint1.y * -y) + 0.5f;
+
+                //RightNeighbour 영점 조절 
+                float xPos2 = (spawnPoint1.x * x) + 5.2f;
+                float yPos2 = (spawnPoint1.y * -y) + 0.5f;
 
                 Vector3 rookieSpwanPoint = new Vector3(xPos, yPos, 0);
 
                 Vector3 neighbourSpwanPoint = new Vector3(xPos1, yPos1, 0);
 
-                if (randomIndex == 0) // Rookie 소환 
+                Vector3 rightNeighbourSpwanPoint = new Vector3(xPos2, yPos2, 0);
+
+                Vector3 leftRookieSpwanPoint = new Vector3(xPos4, yPos4, 0);
+
+
+                if (randomSpawn1 < 3)
                 {
-                    if (randomRL == 0)
+                    if (randomIndex == 0) // Rookie 소환 
                     {
-                        //Vector3 rightMonsterSpwaner = rookieSpwanPoint + Vector3.right * spacingRight;
-                        //Instantiate(rookiePreafab_R, rightMonsterSpwaner, Quaternion.identity);
-                        //Vector3 leftMonsterSpwaner = rookieSpwanPoint + Vector3.right * spacingRight;
-                        //Instantiate(rookiePreafab_L, rightMonsterSpwaner, Quaternion.identity);
+                        if (randomRL == 0)
+                        {
+                            //Vector3 rightMonsterSpwaner = rookieSpwanPoint + Vector3.right * spacingRight;
+                            //Instantiate(rookiePreafab_R, rightMonsterSpwaner, Quaternion.identity);
+                            //Vector3 leftMonsterSpwaner = rookieSpwanPoint + Vector3.right * spacingRight;
+                            //Instantiate(rookiePreafab_L, rightMonsterSpwaner, Quaternion.identity);
 
-                        //Vector3 leftMonsterSpwaner = rookieSpwanPoint + Vector3.left * spacingRight;
-                        //Instantiate(rookiePreafab_R, leftMonsterSpwaner, Quaternion.identity);
+                            //Vector3 leftMonsterSpwaner = rookieSpwanPoint + Vector3.left * spacingRight;
+                            //Instantiate(rookiePreafab_R, leftMonsterSpwaner, Quaternion.identity);
 
 
-                        Vector3 rightMonsterSpwaner = rookieSpwanPoint + Vector3.right * spacingRight;
-                        Instantiate(rookiePreafab_R, rightMonsterSpwaner, Quaternion.identity);
+                            Vector3 rightMonsterSpwaner = rookieSpwanPoint + Vector3.right * spacingRight;
+                            Instantiate(rookiePreafab_L, rightMonsterSpwaner, Quaternion.identity);
 
-                        Vector3 leftMonsterSpwaner = rookieSpwanPoint + Vector3.left * spacingRight;
-                        Instantiate(rookiePreafab_L, leftMonsterSpwaner, Quaternion.identity);
-                        //RookieLeftPrefabFlip();
+                            Vector3 leftMonsterSpwaner = leftRookieSpwanPoint + Vector3.left * spacingRight;
+                            Instantiate(rookiePreafab_R, leftMonsterSpwaner, Quaternion.identity);
+                            //RookieLeftPrefabFlip();
+                            //RookieLeftPrefabFlip();
 
+
+                        }
+                        else if (randomRL == 1)
+                        {
+                            Vector3 rightMonsterSpwaner = rookieSpwanPoint + Vector3.right * spacingRight;
+                            Instantiate(rookiePreafab_L, rightMonsterSpwaner, Quaternion.identity);
+
+
+                            Vector3 leftMonsterSpwaner = leftRookieSpwanPoint + Vector3.left * spacingRight;
+                            Instantiate(rookiePreafab_R, leftMonsterSpwaner, Quaternion.identity);
+                            //RookieLeftPrefabFlip();
+                            //RookieLeftPrefabFlip();
+
+                        }
                     }
-                    else if (randomRL == 1)
+                    else if (randomIndex == 1) // 이웃 
                     {
-                        Vector3 rightMonsterSpwaner = rookieSpwanPoint + Vector3.right * spacingRight;
-                        Instantiate(rookiePreafab_R, rightMonsterSpwaner, Quaternion.identity);
+                        if (randomRL == 0)
+                        {
+                            Vector3 rightMonsterSpwaner1 = rightNeighbourSpwanPoint + Vector3.right * spacingRight;
+                            Instantiate(neighbourPreafab_L, rightMonsterSpwaner1, Quaternion.identity);
 
+                            Vector3 leftMonsterSpwaner1 = neighbourSpwanPoint + Vector3.left * spacingRight;
+                            Instantiate(neighbourPreafab_R, leftMonsterSpwaner1, Quaternion.identity);
+                        }
+                        else if (randomRL == 1)
+                        {
 
-                        Vector3 leftMonsterSpwaner = rookieSpwanPoint + Vector3.left * spacingRight;
-                        Instantiate(rookiePreafab_L, leftMonsterSpwaner, Quaternion.identity);
-                        //RookieLeftPrefabFlip();
+                            Vector3 rightMonsterSpwaner1 = rightNeighbourSpwanPoint + Vector3.right * spacingRight;
+                            Instantiate(neighbourPreafab_L, rightMonsterSpwaner1, Quaternion.identity);
 
-
+                            Vector3 leftMonsterSpwaner1 = neighbourSpwanPoint + Vector3.left * spacingRight;
+                            Instantiate(neighbourPreafab_R, leftMonsterSpwaner1, Quaternion.identity);
+                        }
                     }
                 }
-                else if (randomIndex == 1) // 이웃 
+                else if (randomSpawn1 >= 3)
                 {
-                    if (randomRL == 0)
-                    {
-                        Vector3 rightMonsterSpwaner1 = neighbourSpwanPoint + Vector3.right * spacingRight;
-                        Instantiate(neighbourPreafab_R, rightMonsterSpwaner1, Quaternion.identity);
 
-                        Vector3 leftMonsterSpwaner1 = neighbourSpwanPoint + Vector3.left * spacingRight;
-                        Instantiate(neighbourPreafab_L, leftMonsterSpwaner1, Quaternion.identity);
-                    }
-                    else if (randomRL == 1)
-                    {
-
-                        Vector3 rightMonsterSpwaner1 = neighbourSpwanPoint + Vector3.right * spacingRight;
-                        Instantiate(neighbourPreafab_R, rightMonsterSpwaner1, Quaternion.identity);
-
-                        Vector3 leftMonsterSpwaner1 = neighbourSpwanPoint + Vector3.left * spacingRight;
-                        Instantiate(neighbourPreafab_L, leftMonsterSpwaner1, Quaternion.identity);
-                    }
                 }
-                
+
             }
-
         }
+
     }
 
-    public void NeighbourPrefabFlip()
+    //public void NeighbourPrefabFlip()
+    //{
+    //    // 현재 스케일을 가져옴
+    //    Vector3 currentScale = neighbourPreafab.transform.localScale;
+
+    //    // x축 스케일을 반전
+    //    Vector3 newScale = new Vector3(-currentScale.x, currentScale.y, currentScale.z);
+
+    //    // 프리팹의 스케일을 수정하여 반전 적용
+    //    neighbourPreafab.transform.localScale = newScale;
+    //}
+
+    //public void RookieLeftPrefabFlip()
+    //{
+    //    // 현재 스케일을 가져옴
+    //    Vector3 currentScale = rookiePreafab_L.transform.localScale;
+
+    //    // x축 스케일을 반전
+    //    Vector3 newScale = new Vector3(-currentScale.x, currentScale.y, currentScale.z);
+
+    //    // 프리팹의 스케일을 수정하여 반전 적용
+    //    rookiePreafab_L.transform.localScale = newScale;
+
+
+    //}
+    public void ImageSc()
     {
-        // 현재 스케일을 가져옴
-        Vector3 currentScale = neighbourPreafab.transform.localScale;
+        Vector3 uiImageScale = uiImage.transform.localScale;
 
-        // x축 스케일을 반전
-        Vector3 newScale = new Vector3(-currentScale.x, currentScale.y, currentScale.z);
+        Vector3 newScale = new Vector3(0.001f, 0.001f, 0);
 
-        // 프리팹의 스케일을 수정하여 반전 적용
-        neighbourPreafab.transform.localScale = newScale;
+        uiImage.transform.localScale = newScale;
     }
 
-    public void RookieLeftPrefabFlip()
+    public void ImageSc1()
     {
-        // 현재 스케일을 가져옴
-        Vector3 currentScale = rookiePreafab_L.transform.localScale;
+        Vector3 uiImageScale = uiImage1.transform.localScale;
 
-        // x축 스케일을 반전
-        Vector3 newScale = new Vector3(-currentScale.x, currentScale.y, currentScale.z);
+        Vector3 newScale = new Vector3(0.001f, 0.001f, 0);
 
-        // 프리팹의 스케일을 수정하여 반전 적용
-        rookiePreafab_L.transform.localScale = newScale;
+        uiImage1.transform.localScale = newScale;
     }
 
+    public void ImageSc2()
+    {
+        Vector3 uiImageScale = uiImage2.transform.localScale;
+
+        Vector3 newScale = new Vector3(0.001f, 0.001f, 0);
+
+        uiImage2.transform.localScale = newScale;
+    }
 
 }
 
