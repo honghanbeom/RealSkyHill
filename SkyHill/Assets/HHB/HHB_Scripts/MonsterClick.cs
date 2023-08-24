569bc7c271217910761b18b85c240d5a510367e1
MYNOTEBOOK\bobby <bobbyhong7@naver.com>
2023-08-24 15:17:11 +0900

    V0.7.1 mergeScene

--- SkyHill/Assets/HHB/HHB_Scripts/MonsterClick.cs
+++ SkyHill/Assets/HHB/HHB_Scripts/MonsterClick.cs
@@ -1,26 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class MonsterClick : MonoBehaviour
-{
-    public MonsterData monsterData;
+//public class MonsterClick : MonoBehaviour
+//{
+//    public MonsterData monsterData;
 
-    private void OnMouseDown()
-    {
-        // 플레이어와 콜라이더 맞다는 조건
-        string gameObjTag = gameObject.tag;
-        if (gameObjTag == "Enemy")
-        {
-            string monsterName = monsterData.MonsterName;
-            float monsterHp = monsterData.MonsterHP;
-            float monsterMaxDamage = monsterData.MonsterMaxDamage;
-            float monstetMidDamage = monsterData.MonsterMidDamage;
-            float monsterMinDamage = monsterData.MonsterMinDamage;
-            float monsterEXP = monsterData.MonsterEXP;
+//    private void OnMouseDown()
+//    {
+//        // 플레이어와 콜라이더 맞다는 조건
+//        string gameObjTag = gameObject.tag;
+//        if (gameObjTag == "Enemy")
+//        {
+//            string monsterName = monsterData.MonsterName;
+//            float monsterHp = monsterData.MonsterHP;
+//            float monsterMaxDamage = monsterData.MonsterMaxDamage;
+//            float monstetMidDamage = monsterData.MonsterMidDamage;
+//            float monsterMinDamage = monsterData.MonsterMinDamage;
+//            float monsterEXP = monsterData.MonsterEXP;
 
-            Fight.fight.MonsterFight(monsterName, monsterHp, monsterMaxDamage, monstetMidDamage,
-                monsterMinDamage, monsterEXP);
-        }
-    }
-}
+//            Fight.fight.MonsterFight(monsterName, monsterHp, monsterMaxDamage, monstetMidDamage,
+//                monsterMinDamage, monsterEXP);
+//        }
+//    }
+//}
--- SkyHill/Assets/HHB/HHB_Scripts/ScriptableObject/MonsterData.cs
+++ SkyHill/Assets/HHB/HHB_Scripts/ScriptableObject/MonsterData.cs
@@ -1,28 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-[CreateAssetMenu (fileName = "Monster Data", menuName = "Scriptable Object/Monster Data",
-    order = int.MaxValue)]
-public class MonsterData : ScriptableObject
-{
-    [SerializeField]
-    private string monsterName;
-    public string MonsterName { get { return monsterName; } }
-    [SerializeField]
-    private float monsterHP;
-    public float MonsterHP { get { return monsterHP; } }
-    [SerializeField]
-    private float monsterMaxDamage;
-    public float MonsterMaxDamage { get { return monsterMaxDamage; } }
-    [SerializeField]
-    private float monsterMidDamage;
-    public float MonsterMidDamage { get { return monsterMidDamage; } }
-    [SerializeField]
-    private float monsterMinDamage;
-    public float MonsterMinDamage { get { return monsterMinDamage; } }
-    [SerializeField]
-    private int monsterEXP;
-    public float MonsterEXP { get { return monsterEXP; } }
+//[CreateAssetMenu (fileName = "Monster Data", menuName = "Scriptable Object/Monster Data",
+//    order = int.MaxValue)]
+//public class MonsterData : ScriptableObject
+//{
+//    [SerializeField]
+//    private string monsterName;
+//    public string MonsterName { get { return monsterName; } }
+//    [SerializeField]
+//    private float monsterHP;
+//    public float MonsterHP { get { return monsterHP; } }
+//    [SerializeField]
+//    private float monsterMaxDamage;
+//    public float MonsterMaxDamage { get { return monsterMaxDamage; } }
+//    [SerializeField]
+//    private float monsterMidDamage;
+//    public float MonsterMidDamage { get { return monsterMidDamage; } }
+//    [SerializeField]
+//    private float monsterMinDamage;
+//    public float MonsterMinDamage { get { return monsterMinDamage; } }
+//    [SerializeField]
+//    private int monsterEXP;
+//    public float MonsterEXP { get { return monsterEXP; } }
 
-}
+//}
